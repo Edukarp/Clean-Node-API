5486c773fdf79426c9da91c346e0367b6a109972
Eduardo Karpfenstein <edukarp10@gmail.com>
2025-04-24 10:19:20 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,24 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SigUp Routes', () => {
+    beforeAll(async () => {
+            if (!process.env.MONGO_URL) {
+                throw new Error('MONGO_URL is not defined')
+            }
+            await MongoHelper.connect(process.env.MONGO_URL)
+        })
+
+        afterAll(async () => {
+            await MongoHelper.disconnect()
+        })
+
+        beforeEach(async () => {
+            const accountCollection = MongoHelper.getCollection('accounts')
+            await accountCollection.deleteMany({})
+        })
+
     test('Should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
@@ -12,5 +29,5 @@ describe('SigUp Routes', () => {
                 passwordConfirmation: '123'
             })
             .expect(200)
-        })
+    })
 })
